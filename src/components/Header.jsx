253fa1e60383f10ffd874d989d855962fa9b253c
Radiy <radiyars@yandex.ru>
2022-08-26 14:13:30 +0300

    Making social network. Step1

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+	return (
+		<header className='header'>
+			<img src='https://e7.pngegg.com/pngimages/356/636/png-clipart-logo-graphic-designer-business-online-and-offline-design-ring-orange.png' />
+		</header>
+	)
+}
+
+export default Header
