354ff37fc564e57fade57d96e9cca03cbcc1af1d
radiyars <radiyars@yandex.ru>
2022-09-14 20:14:09 +0300

    Added content for Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,34 @@
-import classes from './Dialogs.module.css';
+import styles from './Dialogs.module.css';
 
 
 function Dialogs(props) {
 	return (
-		<div>
-			Dialogs bla bla
-			sadfasdf
-			asdfas
-			fasdf
-			asdf
-
+		<div className={styles.dialogs}>
+			<div className={styles.dialogsItems}>
+				<div className={styles.dialog + ' ' + styles.active}>
+					Dimi4
+				</div>
+				<div className={styles.dialog}>
+					Andrey
+				</div>
+				<div className={styles.dialog}>
+					Sveta
+				</div>
+				<div className={styles.dialog}>
+					Sasha
+				</div>
+				<div className={styles.dialog}>
+					Viktor
+				</div>
+				<div className={styles.dialog}>
+					Valera
+				</div>
+			</div>
+			<div className={styles.messages}>
+				<div className={styles.message}>Hi</div>
+				<div className={styles.message}>How is your React?</div>
+				<div className={styles.message}>Yo!</div>
+			</div>
 		</div>
 	)
 }
