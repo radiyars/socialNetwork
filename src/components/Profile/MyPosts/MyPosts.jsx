92d135d653a61e77ef7eebaf25697e8b92ad615b
radiyars <radiyars@yandex.ru>
2022-11-02 09:58:08 +0300

    Методы работы в сданными в store вынесены в одну функцию dispatch

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,12 +11,12 @@ function MyPosts(props) {
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
-		props.addPost();
+		props.dispatch({ type: 'ADD-POST' });
 	}
 
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		props.updateNewPostText(text);
+		props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text });
 	}
 
 	return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -31,33 +31,35 @@ let store = {
 		}
 	},
 
-	getState() {
-		return this._state
-	},
-
 	_callSubscriber() {
 		console.log('State changed');
-	},
-
-	addPost() {
-		let newPost = {
-			id: 5,
-			post: this._state.profilePage.newPostText,
-			likesCount: 0
-		};
-		this._state.profilePage.posts.push(newPost);
-		this._state.profilePage.newPostText = '';
-		this._callSubscriber(this._state);
-	},
-
-	updateNewPostText(newText) {
-		this._state.profilePage.newPostText = newText;
-		this._callSubscriber(this._state);
 	},
 
 	subscribe(observer) {
 		this._callSubscriber = observer; // паттерн observer
 	},
+
+	getState() {
+		return this._state
+	},
+
+
+	dispatch(action) {
+		if (action.type === 'ADD-POST') {
+			let newPost = {
+				id: 5,
+				post: this._state.profilePage.newPostText,
+				likesCount: 0
+			};
+			this._state.profilePage.posts.push(newPost);
+			this._state.profilePage.newPostText = '';
+			this._callSubscriber(this._state);
+		}
+		else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+			this._state.profilePage.newPostText = action.newText;
+			this._callSubscriber(this._state);
+		}
+	}
 
 }
 
