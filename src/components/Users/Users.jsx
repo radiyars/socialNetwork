e1faa838d7a79df1dd082a782a68e6ac0b0dd928
radiyars <radiyars@yandex.ru>
2022-11-20 21:08:31 +0300

    Добавить/Удалить друга теперь делается через соответствующие endpoint у axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from './Users.module.css';
 import userPhoto from '../../assets/images/userNullAvatar.png'
 import { NavLink } from "react-router-dom";
+import axios from 'axios';
+
 
 let Users = (props) => {
 
@@ -36,8 +38,34 @@ let Users = (props) => {
 					</div>
 					<div>
 						{user.followed
-							? <button onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
-							: <button onClick={() => { props.follow(user.id) }}>Follow</button>}
+
+							? <button onClick={() => {
+
+								axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`,
+									{
+										withCredentials: true,
+										headers: { 'API-KEY': 'c7b45913-93c3-498c-9d8d-dac7cb1512dc' }
+									})
+									.then(response => {
+										if (response.data.resultCode == 0) {
+											props.unfollow(user.id)
+										}
+									});
+							}}>Unfollow</button>
+
+							: <button onClick={() => {
+
+								axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {},
+									{
+										withCredentials: true,
+										headers: { 'API-KEY': 'c7b45913-93c3-498c-9d8d-dac7cb1512dc' }
+									})
+									.then(response => {
+										if (response.data.resultCode == 0) {
+											props.follow(user.id)
+										}
+									});
+							}}>Follow</button>}
 					</div>
 				</span>
 				<span>
@@ -50,7 +78,7 @@ let Users = (props) => {
 						<div>{'user.location.city'}</div>
 					</span>
 				</span>
-			</div>
+			</div >
 			)
 		}
 	</div >
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,7 +11,8 @@ class UsersContainer extends React.Component {
 	// полсе того как компонента прорисовалась можно выполнить запрос на сервер
 	componentDidMount() {
 		this.props.toggleIsFetching(true);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+			{ withCredentials: true })
 			.then(response => {
 				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
@@ -22,7 +23,7 @@ class UsersContainer extends React.Component {
 	onPageChenged = (pageNumber) => {
 		this.props.toggleIsFetching(true);
 		this.props.setCurrentPage(pageNumber);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, { withCredentials: true })
 			.then(response => {
 				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
